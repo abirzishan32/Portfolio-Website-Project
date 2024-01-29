775115e5f75afa3c4c068da6a43e41de704eae10
abir_zishan <118625188+abirzishan32@users.noreply.github.com>
2024-01-30 05:27:29 +0600

    Update main.js

--- main.js
+++ main.js
@@ -17,3 +17,25 @@ navToggle.addEventListener('click', () => {
 navClose.addEventListener('click', () => {
     navMenu.classList.remove('active');
 });
+
+
+const sections = document.querySelectorAll('section');
+
+window.addEventListener('scroll', () => {
+    const scrollPosition = window.scrollY;
+
+    sections.forEach(section => {
+        const top = section.offsetTop - 50;
+        const bottom = top + section.clientHeight;
+
+        if (scrollPosition >= top && scrollPosition < bottom) {
+
+            const navLink = document.querySelector('.nav__link[href="#home"]');
+            navLink.classList.add('active-link');
+        } else {
+
+            const navLink = document.querySelector('.nav__link[href="#education"]');
+            navLink.classList.remove('active-link');
+        }
+    });
+});
