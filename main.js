eeb3aa969b6dfc656abd27ea0bec913b3956baa5
abir_zishan <118625188+abirzishan32@users.noreply.github.com>
2024-01-30 04:44:57 +0600

    Update main.js

--- main.js
+++ main.js
@@ -5,3 +5,15 @@ function toggleExpansion(button) {
     const additionalText = card.querySelector('.additional-text');
     additionalText.style.display = additionalText.style.display === 'block' ? 'none' : 'block';
 }
+
+const navToggle = document.getElementById('nav-toggle');
+const navClose = document.getElementById('nav-close');
+const navMenu = document.getElementById('nav-menu');
+
+navToggle.addEventListener('click', () => {
+    navMenu.classList.toggle('active');
+});
+
+navClose.addEventListener('click', () => {
+    navMenu.classList.remove('active');
+});
